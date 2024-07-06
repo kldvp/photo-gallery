469bec5fb500074ce385d8e36fd5bc7fd33c1ac2
kldvp <prasadkodali786@gmail.com>
2024-07-06 20:40:47 +0530

    fix: only allow specified origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'https://photo-uploader.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
